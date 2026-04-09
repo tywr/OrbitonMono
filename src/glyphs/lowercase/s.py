26e4d81a282c1da8de3dad0b3d9467eea9c3fc3c
tywr <tanguy.viv@gmail.com>
2026-04-09 22:37:48 +0200

    refactor: lowercase s

--- src/glyphs/lowercase/s.py
+++ src/glyphs/lowercase/s.py
@@ -1,13 +1,14 @@
 from glyphs import Glyph
 from draw.superellipse_loop import draw_superellipse_loop
 from draw.cross_curve import draw_cross_curve
+from draw.corner import draw_corner
 
 
 class LowercaseSGlyph(Glyph):
     name = "lowercase_s"
     unicode = "0x73"
     offset = 0
-    loop_ratio = 0.6  # Controls the height of each half-loop
+    loop_ratio = 0.5  # Controls the height of each half-loop
 
     def draw(self, pen, dc):
         b = dc.body_bounds(
@@ -20,9 +21,9 @@ class LowercaseSGlyph(Glyph):
         hx, hy = b.hx, b.hy * self.loop_ratio
 
         # Height of each half-loop from its respective baseline
-        loop_len = b.y2 * self.loop_ratio
-        ym1 = b.y1 + loop_len - dc.stroke_y / 2  # Top of the bottom half-loop
-        ym2 = b.y2 - loop_len + dc.stroke_y / 2  # Bottom of the top half-loop
+        lh = b.height * self.loop_ratio
+        ym1 = b.y1 + lh + dc.stroke_y / 2
+        ym2 = b.y2 - lh - dc.stroke_y / 2
 
         # Bottom half-loop (cut at top)
         draw_superellipse_loop(
@@ -32,16 +33,30 @@ class LowercaseSGlyph(Glyph):
         draw_superellipse_loop(
             pen, dc.stroke_x, dc.stroke_y, b.x1, ym2, b.x2, b.y2, hx, hy, cut="bottom"
         )
-        # Middle cross junction connecting the two half-loops
-        draw_cross_curve(
+
+        # Middle left
+        draw_corner(
             pen,
             dc.stroke_x,
             dc.stroke_y,
             b.x1,
-            (b.y1 + ym1) / 2,
+            ym2 + (b.y2 - ym2) / 2,
+            b.xmid,
+            ym2,
+            hx,
+            hy,
+        )
+        
+        # Middle right
+        draw_corner(
+            pen,
+            dc.stroke_x,
+            dc.stroke_y,
             b.x2,
-            (b.y2 + ym2) / 2,
-            b.hx,
-            self.loop_ratio * b.hy,
-            invert=True,
+            b.y1 + (ym1 - b.y1) / 2,
+            b.xmid,
+            ym1,
+            hx,
+            hy,
+            orientation="top-left"
         )
