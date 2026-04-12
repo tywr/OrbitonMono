0687a2aa2601d242443aea8c172e3d8f884408b8
tywr <tanguy.viv@gmail.com>
2026-04-12 21:22:04 +0200

    adjust: wider D

--- src/glyphs/uppercase/d.py
+++ src/glyphs/uppercase/d.py
@@ -7,6 +7,7 @@ class UppercaseDGlyph(UppercaseGlyph):
     name = "uppercase_d"
     unicode = "0x44"
     offset = 0
+    width_ratio=1.12
 
     def draw(self, pen, dc):
         b = dc.body_bounds(
