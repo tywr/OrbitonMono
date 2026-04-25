c9cde98c14c1019b7893e832661437dfaf5914e1
tywr <tanguy.viv@gmail.com>
2026-04-25 13:17:11 +0200

    add: smooth tilde

--- src/glyphs/special/tilde.py
+++ src/glyphs/special/tilde.py
@@ -14,50 +14,35 @@ class TildeGlyph(Glyph):
     unicode = "0x7E"
     offset = 0
     height_ratio = 0.25
-    corner_ratio = 0.12
-    width_ratio = 1.1
-    stroke_ratio = 1.5
-    hx_ratio = 0.5
-    hy_ratio = 0.5
+    x_corner_ratio = 0.24
+    y_corner_offset = 0.075
+    width_ratio = 1.2
+    stroke_x_ratio = 1
+    stroke_y_ratio = 1.2
+    hy_ratio = 0.8
 
     def draw(self, pen, dc):
         b = dc.body_bounds(
             offset=self.offset, height="x_height", width_ratio=self.width_ratio
         )
-        s = dc.stroke_x * self.stroke_ratio
-        y1 = dc.math - self.height_ratio * b.height / 2 - s / 2
-        y2 = dc.math + self.height_ratio * b.height / 2 + s / 2
-        xi1 = b.x1 + self.corner_ratio * b.width + s / 2
-        xi2 = b.x2 - self.corner_ratio * b.width - s / 2
+        sx, sy = dc.stroke_x * self.stroke_x_ratio, dc.stroke_x * self.stroke_x_ratio
+        hy = b.hy * self.hy_ratio
+        h = self.height_ratio * b.height
+        y1 = dc.math - h / 2 - sy / 2
+        y2 = dc.math + h / 2 + sy / 2
+        xi1 = b.x1 + self.x_corner_ratio * b.width + sx / 2
+        xi2 = b.x2 - self.x_corner_ratio * b.width - sx / 2
+        yi1 = y1 + h * self.y_corner_offset
+        yi2 = y2 - h * self.y_corner_offset
+        ihx = (xi2 - xi1) / 2 - sy / 4
+        ehx = (xi2 - xi1) / 2 + sy / 4
+        ihy = b.hy * (y2 - y1) * (1 - self.y_corner_offset) / b.height
 
-        draw_parallelogramm_vertical(
-            pen, s, s, b.x1, y1, xi1, y2, direction="top-right", delta=s
-        )
-        draw_rect(
-            pen,
-            xi1,
-            y2 - s,
-            xi1 + dc.gap,
-            y2,
-        )
-        draw_parallelogramm_vertical(
-            pen,
-            s,
-            s,
-            xi1 + dc.gap,
-            y2,
-            xi2 - dc.gap,
-            y1,
-            delta=s,
-            direction="bottom-right",
-        )
-        draw_rect(
-            pen,
-            xi2 - dc.gap,
-            y1,
-            xi2,
-            y1 + s,
-        )
-        draw_parallelogramm_vertical(
-            pen, s, s, xi2, y1, b.x2, y2, direction="top-right", delta=s
-        )
+        pen.moveTo((b.x1, yi1))
+        pen.curveTo((b.x1, yi1 + hy), (xi1 - ehx, y2), (xi1, y2))
+        pen.curveTo((xi1 + ehx, y2), (xi2 - ihx, y1 + sy), (xi2, y1 + sy))
+        pen.curveTo((xi2 + ihx, y1 + sy), (b.x2 - sx, y2 - ihy), (b.x2 - sx, yi2))
+        pen.lineTo((b.x2, yi2))
+        pen.curveTo((b.x2, yi2 - hy), (xi2 + ehx, y1), (xi2, y1))
+        pen.curveTo((xi2 - ehx, y1), (xi1 + ihx, y2 - sy), (xi1, y2 - sy))
+        pen.curveTo((xi1 - ihx, y2 - sy), (b.x1 + sx, yi1 + ihy), (b.x1 + sx, yi1))
