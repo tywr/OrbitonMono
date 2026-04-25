bdce45f13b63e36d31135eba5cb48f8fa11e4e03
tywr <tanguy.viv@gmail.com>
2026-04-25 13:17:17 +0200

    fix: solidus heights

--- src/glyphs/special/reverse_solidus.py
+++ src/glyphs/special/reverse_solidus.py
@@ -7,19 +7,21 @@ class ReverseSolidusGlyph(Glyph):
     unicode = "0x5C"
     offset = 0
     width_ratio = 1
+    height_ratio = 0.8
 
     def draw(self, pen, dc):
         b = dc.body_bounds(
             offset=self.offset, height="ascent", width_ratio=self.width_ratio
         )
         ymid = dc.parenthesis
+        h = self.height_ratio * dc.parenthesis_length
         draw_parallelogramm(
             pen,
             dc.stroke_x,
             dc.stroke_y,
             b.x2,
-            ymid - dc.parenthesis_length / 2,
+            ymid - h / 2,
             b.x1,
-            ymid + dc.parenthesis_length / 2,
+            ymid + h / 2,
             direction="top-left"
         )
--- src/glyphs/special/solidus.py
+++ src/glyphs/special/solidus.py
@@ -7,18 +7,20 @@ class SolidusGlyph(Glyph):
     unicode = "0x2F"
     offset = 0
     width_ratio = 1
+    height_ratio = 0.8
 
     def draw(self, pen, dc):
         b = dc.body_bounds(
             offset=self.offset, height="ascent", width_ratio=self.width_ratio
         )
         ymid = dc.parenthesis
+        h = self.height_ratio * dc.parenthesis_length
         draw_parallelogramm(
             pen,
             dc.stroke_x,
             dc.stroke_y,
             b.x1,
-            ymid - dc.parenthesis_length / 2,
+            ymid - h / 2,
             b.x2,
-            ymid + dc.parenthesis_length / 2,
+            ymid + h / 2,
         )
