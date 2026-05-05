8e4fbbc19a11dfde593d9479a972d61d6c956f62
tywr <tanguy.viv@gmail.com>
2026-05-05 17:14:49 +0200

    update: lowercase r

--- src/glyphs/lowercase/r.py
+++ src/glyphs/lowercase/r.py
@@ -1,18 +1,16 @@
 from glyphs import Glyph
 from draw.arch import draw_arch
 from draw.rect import draw_rect
-from draw.polygon import draw_polygon
 
 
 class LowercaseRGlyph(Glyph):
     name = "lowercase_r"
     unicode = "0x72"
     offset = 10
-    loop_ratio = 0.92
-    hx_ratio = 1.05
+    loop_ratio = 0.9
+    hx_ratio = 1.00
     hy_ratio = 1
     taper = 0.5
-    ending_thickness = 0.8
     width_ratio = 1
     tail_offset = 0.03
 
@@ -26,7 +24,7 @@ class LowercaseRGlyph(Glyph):
         ys = b.y2 - self.loop_ratio * b.height
         xt = b.x2 - self.tail_offset * b.width
 
-        arch_params = draw_arch(
+        draw_arch(
             pen,
             dc.stroke_x,
             dc.stroke_y,
